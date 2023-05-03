f78a70c23ed027c1c67fac14f07b053a6bcbffa2
turujpa <walter.ti.chef@hotmail.com>
2023-05-03 22:15:11 +0000

    Apache v.1.0

---
+++ script-iac2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo  "Atualização do servidor... "
+
+apt-get update
+apt-get upgrade -y
+apt-get install apacher2  -y
+apt-get install unzip -y
+
+echo "Baixando arquivos da aplicação..."
+
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
+
+
